447a24010ce6d9e758c82bd2e306c903ff0947ab
Dhruv-M-Sathavara <dhruvms1234@gmail.com>
2025-01-21 23:26:46 +0530

    edit button worcable

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -141,23 +141,26 @@ class _HommepaggeState extends State<Hommepagge> {
                     
                     ),
                     Container(
-                      padding: EdgeInsets.symmetric(horizontal: 22,vertical: 5),
-                      decoration: BoxDecoration(
-                        color: Colors.greenAccent,
-                        borderRadius: BorderRadius.circular(20)
-                      ),
-                      child: GestureDetector(
-                        onTap: (){
+                          padding: EdgeInsets.symmetric(horizontal: 22, vertical: 5),
+                          decoration: BoxDecoration(
+                            color: Colors.greenAccent,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: GestureDetector(
+                            onTap: () {
+                              openEditBox(docsnap);
+                            },
+                            child: Text(
+                              "Edit",
+                              style: TextStyle(
+                                fontSize: 15,
+                                color: Colors.white,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ),
+                        ),
 
-                        },
-                        child: Text("Edit",style: TextStyle(
-                          fontSize: 15,
-                          color: Colors.white,
-                          fontWeight: FontWeight.w500
-                        ),),
-                      ),
-                    
-                    ),
                     Container(
                       padding: EdgeInsets.symmetric(horizontal: 12,vertical: 5),
                       decoration: BoxDecoration(
@@ -479,6 +482,154 @@ Future OpenBox() {
                       },
                       child: Text(
                         "Add",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
+Future openEditBox(DocumentSnapshot docsnap) {
+  DateTime? selectedDate = DateTime.now();
+  TextEditingController titleController = TextEditingController(text: docsnap["title"]);
+  TextEditingController subtitleController = TextEditingController(text: docsnap["Subtitle"]);
+  TextEditingController dateController = TextEditingController(text: docsnap["Due_Date"]);
+
+  return showDialog(
+    context: context,
+    builder: (BuildContext context) => AlertDialog(
+      content: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: Icon(Icons.cancel),
+                  ),
+                  SizedBox(width: 60),
+                  Text(
+                    'Edit Task',
+                    style: TextStyle(
+                      color: Colors.blue.shade400,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              // Title Field
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black, width: 2),
+                ),
+                child: TextField(
+                  controller: titleController,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Title',
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              // Subtitle Field
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black, width: 2),
+                ),
+                child: TextField(
+                  controller: subtitleController,
+                  decoration: InputDecoration(
+                    hintText: 'Subtitle',
+                    border: InputBorder.none,
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              
+              TableCalendar(
+                firstDay: DateTime.utc(2000),
+                lastDay: DateTime.utc(2100),
+                focusedDay: selectedDate!,
+                calendarFormat: CalendarFormat.month,
+                selectedDayPredicate: (day) {
+                  return isSameDay(selectedDate, day);
+                },
+                onDaySelected: (selectedDay, focusedDay) {
+                  setState(() {
+                    selectedDate = selectedDay;
+                    dateController.text =
+                        "${selectedDay.day}-${selectedDay.month}-${selectedDay.year}";
+                  });
+                },
+                headerStyle: HeaderStyle(
+                  formatButtonVisible: false,
+                  titleCentered: true,
+                ),
+              ),
+              SizedBox(height: 20),
+              
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black, width: 2),
+                ),
+                child: TextField(
+                  controller: dateController,
+                  decoration: InputDecoration(
+                    hintText: 'Selected Due Date',
+                    border: InputBorder.none,
+                  ),
+                  readOnly: true,
+                ),
+              ),
+              SizedBox(height: 20),
+              Center(
+                child: Container(
+                  width: 100,
+                  height: 30,
+                  decoration: BoxDecoration(
+                    color: Colors.blue.shade300,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Center(
+                    child: GestureDetector(
+                      onTap: () {
+                        if (selectedDate != null) {
+                          Map<String, dynamic> updatedTask = {
+                            "title": titleController.text,
+                            "Subtitle": subtitleController.text,
+                            "Due_Date": dateController.text,
+                          };
+
+                          DBService().updateTask(
+                            docsnap["id"],
+                            Personal ? "Personal" : Work ? "Workplace" : "Shopping",
+                            updatedTask,
+                            titleController.text,
+                          );
+
+                          Navigator.pop(context);
+                        } else {
+                          print("No date selected!");
+                        }
+                      },
+                      child: Text(
+                        "Update",
                         style: TextStyle(
                           color: Colors.white,
                         ),
